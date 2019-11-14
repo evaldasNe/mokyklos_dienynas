6e4a439db725cac161556e398e421ff330388493
R3purg <marcius989@gmail.com>
2019-11-14 15:15:01 +0200

    created parentUpdate branch

---
+++ src/Controller/ParentController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/parent")
+ */
+class ParentController extends AbstractController
+{
+    /**
+     * @Route("/teacherList", name="parent_teacherList")
+     */
+    public function showAllTeachers(UserRepository $userRepository)
+    {
+        return $this->render('parent/teacherList.html.twig', [
+            'users' => $userRepository->findByRole('TEACHER')
+        ]);
+    }
+
+    /**
+     * @Route("/studentRemark", name="parent_studentRemark")
+     */
+    public function showStudentRemark(UserRepository $userRepository)
+    {
+        return $this->render('parent/studentRemark.html.twig', [
+            'users' => $userRepository->findByRole('STUDENT')
+        ]);
+    }
+
+    /**
+     * @Route("/studentHomework", name="parent_studentHomework")
+     */
+    public function showStudentHomework(UserRepository $userRepository)
+    {
+        return $this->render('parent/studentHomework.html.twig', [
+            'users' => $userRepository->findByRole('STUDENT')
+        ]);
+    }
+
+    /**
+     * @Route("/studentGradeList", name="parent_studentGradeList")
+     */
+    public function showStudentGradeList(UserRepository $userRepository)
+    {
+        return $this->render('parent/studentGradeList.html.twig', [
+            'users' => $userRepository->findByRole('STUDENT'),
+        ]);
+    }
+
+    /**
+     * @Route("/messenger", name="parent_messenger")
+     */
+    public function Messenger(UserRepository $userRepository)
+    {
+        return $this->render('student/messenger.html.twig', [
+            'users' => $userRepository->findByRole('PARENT'),
+        ]);
+    }
+
+    /**
+     * @Route("/studentTimetable", name="parent_studentTimetable")
+     */
+    public function showStudentTimetable(UserRepository $userRepository)
+    {
+        return $this->render('student/studentTimetable.html.twig', [
+            'users' => $userRepository->findByRole('STUDENT'),
+        ]);
+    }
+}
